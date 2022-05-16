6f4be9ccc3cacf3bc9512659987322b29b47a41f
Dinma-Faith <phaytep@gmail.com>
2022-05-16 16:40:58 +0100

    SetUp webpack

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    static: { directory: path.resolve(__dirname, './dist'), },
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 5500,
+  },
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./src/template.html"), // template file
+      filename: "index.html", // output file
+    }),
+
+  ],
+  module: {
+
+    rules: [
+        // CSS, PostCSS, and Sass
+        {
+          test: /\.(scss|css)$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      // Images
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+       // Fonts and SVGs
+       {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+};
