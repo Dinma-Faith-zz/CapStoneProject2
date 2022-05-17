5270e63032528f3dfd0ab8d1001030ab6357e16d
Dinma-Faith <phaytep@gmail.com>
2022-05-17 17:36:10 +0100

    Create homepage

---
+++ src/module/home.js
@@ -0,0 +1,19 @@
+import { url, displayHome } from './variables.js'
+
+export const display = async () => {
+  const fetchData = await fetch(`${url}`);
+  const data = await fetchData.json();
+  displayHome.innerHTML = data.meals
+  .slice(0,6)
+    .map(
+      (meal) => `<div>
+  <img src="${meal.strMealThumb}">
+  <p class="meals">${meal.strMeal}</p>
+  <i class="fa fa-thumbs-up"></i>
+  <button>Comments</button>
+  </div>`
+    )
+};
+
+window.addEventListener("load", display);
+
---
+++ src/module/variables.js
@@ -0,0 +1,2 @@
+export const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=f';
+export const displayHome = document.querySelector('.display-home');
