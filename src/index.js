6cac110846c5da7e01897a9e7848867c0aacc71c
EidHachem <eidhachem1@gmail.com>
2022-05-18 18:46:37 +0300

    Add function to get comments for each meal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import display from './module/home.js';
 import displayPopup from './module/popup.js';
+import getComments from './module/comments.js';
 
 display();
 
@@ -9,5 +10,7 @@ window.addEventListener('click', (e) => {
   if (e.target.parentElement.id === e.target.id) {
     const i = e.target.id;
     displayPopup(i);
+    getComments(i);
+    console.log(e.target.id);
   }
 });
