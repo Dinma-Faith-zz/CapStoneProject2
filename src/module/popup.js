20a51025b6019b078091072cc43257f916279086
EidHachem <eidhachem1@gmail.com>
2022-05-19 19:21:18 +0300

    Add comment counter function

--- src/module/popup.js
+++ src/module/popup.js
@@ -1,8 +1,15 @@
-import {
-  url, modal, overlay, involvmentAPI,
-} from './variables.js';
+import { url, modal, overlay, involvmentAPI } from './variables.js';
 
 const popupArray = [52802, 52815, 52835, 52896, 52903, 52906];
+
+export const commentsCounter = async (i) => {
+  const response = await fetch(`${involvmentAPI}/apps/wC4fHH83VpkiJclfAEX8/comments?item_id=${+i}`);
+  const data = await response.json();
+  const arr = Array.from(data);
+
+  const commentsCounter = modal.querySelector('.comments-counter');
+  commentsCounter.innerHTML = `(${arr.length})`;
+};
 
 export const getComments = async (i) => {
   const response = await fetch(`${involvmentAPI}/apps/wC4fHH83VpkiJclfAEX8/comments?item_id=${+i}`);
@@ -23,7 +30,7 @@ export const postComments = async (i, comment) => {
   await fetch(`${involvmentAPI}/apps/wC4fHH83VpkiJclfAEX8/comments`, {
     method: 'POST',
     headers: {
-      'Content-type': 'application/json',
+      'Content-type': 'application/json;charset=UTF-8',
     },
     body: JSON.stringify(comment),
   });
@@ -35,6 +42,7 @@ const displayPopup = async (i) => {
   const data = await fetchData.json();
   modal.classList.add('open');
   overlay.classList.add('open');
+  const counter = commentsCounter();
   modal.innerHTML = data.meals
     .map(
       (meal) => `<div class="popup-container">
@@ -46,9 +54,8 @@ const displayPopup = async (i) => {
     <p class="popup-meal-details">Country: "${meal.strArea}"</P>
     <p class="popup-meal-details">Prep: <a href="${meal.strYoutube}">Watch video</a></P>
 </div>
-      <div id="${i}" class="comments-header"><h2>Comments</h2></div>
+      <div id="${i}" class="comments-header"><h2>Comments <span class="comments-counter"></span></h2></div>
       <div class="all-comments">
-      
       </div>
       <h2 class="add-comments-header">Add a comment</h2>
       <form class="comments-form">
@@ -59,8 +66,7 @@ const displayPopup = async (i) => {
   </div>`,
     )
     .join('');
-
-  getComments(i);
+  commentsCounter(i);
 };
 
 function closeModal() {
