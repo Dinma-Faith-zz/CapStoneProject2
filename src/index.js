c4c9b1172c690537ae4ae70ccc56093d18205b81
Dinma-Faith <phaytep@gmail.com>
2022-05-19 21:30:59 +0100

    Remove unused comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import './style.css';
-// import displayPopup from './module/popup.js';
-// import { createLike, getLikes } from './module/likes.js';
 import displayPopup, { getComments, postComments } from './module/popup.js';
 import display from './module/home.js';
-// import { itemCounter } from './module/ItemsCounter';
-// import getComments, { postComments } from './module/comments.js'
 
 display();
 
@@ -33,17 +29,5 @@ window.addEventListener('submit', (e) => {
     e.target.reset();
     postComments(i, comment);
     displayPopup(i);
-    // commentsCounter(i);
   }
 });
-
-// window.addEventListener('click', (e) => {//
-//   if (!e.target.matches('.fa-thumbs-up')) return;
-//   const i = e.target.parentElement.id;
-//   const like = {
-//     'item_id': `${i}`,
-//   };
-//   createLike(i, like);
-//   getLikes();
-//   console.log(getLikes);
-// });
