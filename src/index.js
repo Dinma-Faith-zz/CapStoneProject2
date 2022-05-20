0ef94bb6d3e8d85749d678e503c520b64dd2e1dc
Dinma-Faith <phaytep@gmail.com>
2022-05-20 15:38:41 +0100

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import './style.css';
 import displayPopup, { getComments, postComments } from './module/popup.js';
 import display from './module/home.js';
 
-
-
 display();
 
 window.addEventListener('click', (e) => {
